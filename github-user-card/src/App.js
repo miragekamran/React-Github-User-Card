315137fa4280509000a5c2766f1fba6671501833
mirage <mirage.kamran.pkt@gmail.com>
2020-04-12 21:34:18 -0700

    project set up

---
+++ github-user-card/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      gitHubUser: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('')
+  }
+  render(){
+     return (
+    <div className="App">
+     
+    </div>
+  );
+  }
+ 
+}
+
+
